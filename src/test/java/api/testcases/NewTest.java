8f3115ee343d5afe1bcb2c2198f29144073dddce
Alok <Alok@LAPTOP-IAR0UUF3>
2025-11-25 16:16:29 +0530

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/test/java/api/testcases/NewTest.java
+++ src/test/java/api/testcases/NewTest.java
@@ -1,9 +1,18 @@
 package api.testcases;
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import com.testautomation.apitesting.utils.BaseTest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class NewTest {
   @Test
   public void f() {
-  }
-}
+	  
+	  String payload = "extends BaseTes";
+	  RestAssured.given().baseUri("").contentType(ContentType.JSON).body(payload).when().post().then()
+	  .assertThat().statusCode(200).body("token",Matchers.notNullValue()).body(JsonSchemaValidator.matchesJsonSchema("C:\\"))
+	  }
